a70bc940f356f3c1e107b3288954ff26e8adc952
ydk1204 <ejrbdi@naver.com>
2022-09-01 22:34:53 +0900

    Sidebar, Navbar 일부 기능 제작

--- src/App.js
+++ src/App.js
@@ -22,10 +22,12 @@ import {
   ColorMapping,
   Editor,
 } from "./pages";
+
+import { useStateContext } from "./contexts/ContextProvider";
 import "./App.css";
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
