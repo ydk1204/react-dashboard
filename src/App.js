ef1c1f10e07bc8728845873437c64cc5c211b86a
ydk1204 <ejrbdi@naver.com>
2022-09-02 13:56:51 +0900

    hompage 배너 제작

--- src/App.js
+++ src/App.js
@@ -61,8 +61,7 @@ const App = () => {
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
             </div>
-          </div>
-
+          
           <div>
             <Routes>
               {/* Dashboard */}
@@ -90,6 +89,7 @@ const App = () => {
               <Route path="/pyramid" element={<Pyramid />} />
               <Route path="/stacked" element={<Stacked />} />
             </Routes>
+            </div>
           </div>
         </div>
       </BrowserRouter>
